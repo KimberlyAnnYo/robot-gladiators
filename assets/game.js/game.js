7088dd1024b908faa557f11337cc65ee0addb3ad
Kimberly West <kdenton993@gmail.com>
2022-02-20 14:08:45 -0600

    created fight function

---
+++ assets/game.js/game.js
@@ -0,0 +1,5 @@
+window.prompt("what is your robot's name?");
+//window.alert("This  is an alert! Javascript is running!");
+
+// this creates a function named "fight"
+window.alert("The fight has begun!");
